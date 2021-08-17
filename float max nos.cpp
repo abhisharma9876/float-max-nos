f93fb7e3b24885276c726be44e0be0cd91778f6e
abhisharma9876 <86251863+abhisharma9876@users.noreply.github.com>
2021-08-17 21:53:37 +0530

    Add files via upload

---
+++ float max nos.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"enter the no's ";
+	cin>>a>>b>>c;
+	
+	if(a>b&&b>c)
+	cout<<a;
+	else if(b>c)
+	cout<<b;
+	else
+	cout<<c;
+	return 0;
+}
